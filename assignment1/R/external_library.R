558f5fbc7de6144e329825c554860efaa7901906
RobTomkies <63220353+RobTomkies@users.noreply.github.com>
2022-07-28 19:50:37 +1200

    Second function for mean range importing external created

---
+++ assignment1/R/external_library.R
@@ -0,0 +1,16 @@
+#' mean_variety
+#'
+#' @param numbers Input vector of numbers to determine the different means of.
+#'
+#' @return Returns 3 componants, the harmonic mean (harmonic_mean), the geometric mean (geometric_mean) and the standard mean (standard_mean)
+#' @export
+#'
+#' @examples mean_variety(numbers = c(1,2,3,4,5))
+mean_variety <- function(numbers){
+  geo <- psych::geometric.mean(numbers)
+  harm <- psych::harmonic.mean(numbers)
+  standard <- mean(numbers)
+  results <- list(harmonic_mean = harm, geometric_mean = geo, standard_mean = standard)
+  return(results)
+}
+
